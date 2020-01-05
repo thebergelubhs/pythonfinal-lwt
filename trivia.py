ed8e24fbb3c2ca57031eccb7dd41c430d5822455
Luke Theberge <luketheberge@Luke-Ts-Air.fios-router.home>
2020-01-05 13:01:40 -0500

    added shell of hangman game, pulled down roster for incoming team

--- trivia.py
+++ trivia.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import random
+
 from sportsreference.nfl.teams import Teams
+from sportsreference.nfl.roster import Roster
 
 def list_team_names(my_year):
 
@@ -12,6 +15,46 @@ def list_team_names(my_year):
 
     for team in allTeams:
         print("%-22s %3s" % (team.name, team.abbreviation))
+
+def run_hangman_game(year):
+    lastAbbrevName = ""
+    while 1:
+        name = input("Team Name (l/list/q/quit/stop): ")
+        name = name.upper()
+
+        if (name == "") or (name == "Q") or (name == "QUIT") or (name == "STOP"):
+            break
+
+        if (name == "L") or (name == "LIST"):
+            list_team_names(year)
+            continue
+
+        print("Getting Roster for %s, year %s" % (name, year))
+
+        # only get roster is not same as last one
+        if lastAbbrevName != name:
+            try:
+                roster = Roster(name, year=year)
+                lastAbbrevName = name
+            except:
+                print("Unknown Team Name Abbreviations, use list to display Team and Abbreviations")
+                continue
+
+        numPlayers = len(roster.players)
+
+        print("Ready to start guessing Players name len=%d ..." % numPlayers)
+
+        # generate a random number between 0 and the number of players on the roster-1
+        index = random.randint(0, numPlayers-1)
+
+        #here we set the secret
+        player = roster.players[index]
+
+        #creates an variable with an single value of space ' '
+        guesses = ' '
+
+        name = player.name
+        print("name is: %s index is: %d" % (name, index))
 
 def run_trivia():
     year=""
@@ -25,7 +68,7 @@ def run_trivia():
         elif (cmd=="l") or (cmd=="list"):
              list_team_names(year)
         elif (cmd=="h") or (cmd=="hangman"):
-             display_results(year)
+             run_hangman_game(year)
         elif (cmd=="y") or (cmd=="year"):
             year = input("year : ")
 
